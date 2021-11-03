73a8c4a869f11ffe2fc077a33fe3aff0160fa611
Corey Fisher <coreyfisher2021@gmail.com>
2021-11-03 18:23:24 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    toDo(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function toDo(todo) {
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = "x"
+  p.textContent = `${todo} `;
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p);
+  btn.addEventListener("click", remove)
+}
+
+function remove(e) {
+  e.target.parentNode.remove();
+}
